ebf39aa10941f2282da519b2706d16f601cfdb2b
Caleb Rodriguez <sirdigo6@gmail.com>
2021-12-14 10:26:38 -0500

    commit tree ff4e053c9d864fdef70c0abf94e531f6a240cc32

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* {emptyOrNaaa(props.userBio)} */}
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
+      {/* <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a> */}
     </div>
   );
 }
+
+
+
+// function emptyOrNaaa(prop) {
+//   if (prop === ""){
+//     return null
+//   } else if (prop === null) {
+//     return null
+//   } else {
+//     return <p>{prop}</p>;
+//   };
+// };
 
 export default About;
